497c6d250c889643c27d5cef9a569836a43d72cf
MausamChaudhary12 <102513903+MausamChaudhary12@users.noreply.github.com>
2023-02-26 10:38:09 +0545

    entering space

--- fyp-class/app/Http/Controllers/FrontendController.php
+++ fyp-class/app/Http/Controllers/FrontendController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
+
 class FrontendController extends Controller
 {
     public function homepage()
